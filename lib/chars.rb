f2fd1f4dedfbbc054d3605d7b4d28abf184565f0
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2022-11-01 22:14:04 -0300

    Unique Char Quantity Ruby

--- lib/chars.rb
+++ lib/chars.rb
@@ -1,4 +1,10 @@
 class Chars
   def unique_quantity(text)
+    lista = text.split(" ")
+    soma = 0
+    for i in 0...lista.length do
+    soma = soma + lista[i].each_char.to_a.uniq.count
+    end
+    soma
   end
 end
